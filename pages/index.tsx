13639e064c146ea1a8a0023d791e4499deff3756
Pedro Oliveira <pluiz@mychirotouch.com>
2022-10-08 14:55:21 -0300

    add style to input

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,10 +15,12 @@ const Home: NextPage = () => {
 
   return (
     <div className='p-4 space-y-4'>
-      <input type="text" value={name} onChange={e => setName(e.target.value)}/>
-      <button
-        className='px-4 py-2 bg-blue-600 text-white font-bold rounded'
-        onClick={handleAdicionar}>Adicionar</button>
+      <div className='space-x-2'>
+        <input className='border rounded px-4 py-2' type="text" value={name} onChange={e => setName(e.target.value)}/>
+        <button
+          className='px-4 py-2 bg-blue-600 text-white font-bold rounded'
+          onClick={handleAdicionar}>Adicionar</button>
+      </div>
 
       <div className='grid grid-cols-4 gap-4'>
         {
